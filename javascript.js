f0ce3fcc72cf904f2f44e7629f6cbf3fd4f7618b
Sara Yared <sarayared@gmail.com>
2025-03-20 22:17:51 -0400

    final code

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
 // ppromt asking for comma sepereated favlors
-//table how many of each fovlor is ordered 
-//counter for each flavor 
+//table how many of each fovlor is ordered
+//counter for each flavor
+
+const userInput = prompt("Please enter a list of what flavors you had");
+const flavors = userInput.split(",");
+
+const flavorCount = {};
+
+flavors.forEach(flavor =>{
+  if (flavorCount[flavor]) {
+    flavorCount[flavor]++;
+  } else {
+    flavorCount[flavor] = 1;
+  }
+})
+  
+
+console.log(flavorCount);
